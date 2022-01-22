18d6c9789f9bdbea90f5873e0298e8af8902194f
HanSeong Lee <jkhanseong@gmail.com>
2022-01-23 07:18:59 +0900

    ADD | Feedback edit container

---
+++ containers/FeedbackEditContainer/index.js
@@ -0,0 +1,31 @@
+import React, {useCallback} from 'react';
+import EditFeedbackForm from "../../components/EditFeedbackForm";
+import {useRouter} from "next/router";
+
+const FeedbackEditContainer = (props) => {
+    const router = useRouter();
+
+    const onSubmit = useCallback((data) => {
+        // TODO: Add onSubmit code
+        console.log('onSubmit', data);
+    }, []);
+
+    const onCancel = useCallback(() => {
+        router.back();
+    }, []);
+
+    const onDelete = useCallback(() => {
+        // TODO: Add onDelete code
+        console.log('onDelete');
+    }, []);
+
+    return (
+        <EditFeedbackForm {...props}
+                          onSubmit={onSubmit}
+                          onCancel={onCancel}
+                          onDelete={onDelete}
+        />
+    );
+};
+
+export default FeedbackEditContainer;
