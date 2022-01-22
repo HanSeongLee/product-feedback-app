47e87fe5f17f1e362cc222221b2d0ffe5486d87e
HanSeong Lee <jkhanseong@gmail.com>
2022-01-22 23:29:31 +0900

    ADD | Feedback card

---
+++ components/FeedbackCard/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from './style.module.scss';
+import ArrowUpIcon from '/public/icons/icon-arrow-up.svg';
+import CommentIcon from '/public/icons/icon-comments.svg';
+
+const FeedbackCard = ({ title, description, categories, upvoteCount,
+                          commentCount }) => {
+    return (
+        <article className={styles.card}>
+            <div className={styles.body}>
+                <h3 className={styles.title}>
+                    {title}
+                </h3>
+                <p className={styles.description}>
+                    {description}
+                </p>
+                <div className={styles.categoryContainer}>
+                    {categories?.map((category, index) => (
+                        <span className={styles.categoryItem}
+                              key={index}
+                        >
+                            {category}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <button className={styles.upvoteButton}>
+                    <ArrowUpIcon className={styles.arrowUpIcon} /> {upvoteCount}
+                </button>
+                <span className={styles.commentCount}>
+                    <CommentIcon className={styles.commentIcon}/> {commentCount}
+                </span>
+            </div>
+        </article>
+    );
+};
+
+export default FeedbackCard;
