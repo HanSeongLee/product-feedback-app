7bd4434340c101e351783fb2f382df170563509e
HanSeong Lee <jkhanseong@gmail.com>
2022-01-24 19:44:08 +0900

    REFACTOR | Feedback card

---
+++ components/Feedback/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from './style.module.scss';
+import ArrowUpIcon from "../../public/icons/icon-arrow-up.svg";
+import CommentIcon from "../../public/icons/icon-comments.svg";
+
+const Feedback = ({ id, title, description, categories,
+                      upvoteCount, commentCount, onUpvote }) => {
+    return (
+        <div className={styles.feedback}>
+            <div className={styles.body}>
+                <h3 className={styles.title}>
+                    {title}
+                </h3>
+                <p className={styles.description}>
+                    {description}
+                </p>
+                <div className={styles.categoryContainer}>
+                    {categories?.map((category, index) => (
+                        <span className={styles.categoryItem}
+                              key={index}
+                        >
+                            {category}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <button className={styles.upvoteButton}
+                        onClick={_ => onUpvote(id)}
+                >
+                    <ArrowUpIcon className={styles.arrowUpIcon} /> {upvoteCount}
+                </button>
+                <span className={styles.commentCount}>
+                    <CommentIcon className={styles.commentIcon}/> {commentCount}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default Feedback;
--- components/FeedbackCard/index.js
+++ components/FeedbackCard/index.js
@@ -1,39 +1,11 @@
 import React from "react";
 import styles from './style.module.scss';
-import ArrowUpIcon from '/public/icons/icon-arrow-up.svg';
-import CommentIcon from '/public/icons/icon-comments.svg';
+import Feedback from "../Feedback";
 
-const FeedbackCard = ({ id, title, description, categories,
-                          upvoteCount, commentCount, onUpvote }) => {
+const FeedbackCard = (props) => {
     return (
         <article className={styles.card}>
-            <div className={styles.body}>
-                <h3 className={styles.title}>
-                    {title}
-                </h3>
-                <p className={styles.description}>
-                    {description}
-                </p>
-                <div className={styles.categoryContainer}>
-                    {categories?.map((category, index) => (
-                        <span className={styles.categoryItem}
-                              key={index}
-                        >
-                            {category}
-                        </span>
-                    ))}
-                </div>
-            </div>
-            <div className={styles.footer}>
-                <button className={styles.upvoteButton}
-                        onClick={_ => onUpvote(id)}
-                >
-                    <ArrowUpIcon className={styles.arrowUpIcon} /> {upvoteCount}
-                </button>
-                <span className={styles.commentCount}>
-                    <CommentIcon className={styles.commentIcon}/> {commentCount}
-                </span>
-            </div>
+            <Feedback {...props} />
         </article>
     );
 };
